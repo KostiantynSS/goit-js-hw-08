97e3af3fba80c86d99bcb601f33a6f3fb4d60326
KostiantynSS <storoghenko94@gmail.com>
2023-07-15 11:26:11 +0300

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const refs = {
+  galleryList: document.querySelector('.gallery'),
+};
+
+const galleryListImages = galleryItems.map(image => {
+  const li = document.createElement('li');
+  li.classList.add('gallery__item');
+  li.innerHTML = `<a class="gallery__link" href="${image.original}">
+    <img class='gallery__image'  src='${image.preview}' alt='${image.description}' /> </a>`;
+
+  return li;
+});
+
+refs.galleryList.append(...galleryListImages);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
